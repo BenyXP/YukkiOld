83fac83eaab5a695f8cd485a1ad9f2bb71b6a36c
BenyXP <103335260+BenyXP@users.noreply.github.com>
2022-08-15 08:34:38 +0700

    Create logs.py

---
+++ Yukki/Core/Logger/logs.py
@@ -0,0 +1,37 @@
+from Yukki import BOT_USERNAME
+from Yukki.Core.Clients.cli import app
+from config import LOG_GROUP_ID
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+
+
+async def LOG_CHAT(message, what):
+    if message.chat.username:
+        chatusername = f"[{message.chat.title}](t.me/{message.chat.username})"
+    else:
+        chatusername = f"{message.chat.title}"
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    mention = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+    logger_text = f"""
+**💡 {what}**
+**🧑‍💼 Pengguna:** {mention}
+**💬 Group:** {chatusername}
+**🆔 Id:** `{chat_id}`
+**✨ Permintaan:** {message.text}
+"""
+    await app.send_message(
+        LOG_GROUP_ID,
+        logger_text,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        f"• {user_name} •",
+                        url=f"{message.link}",
+                    )
+                ]
+            ]
+        ),
+    )
